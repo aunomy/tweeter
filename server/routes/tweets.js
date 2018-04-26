9b015691922530591c462d768a780d7bfa2e1bc9
Andrea <finlayandi@gmail.com>
2018-04-26 18:25:08 +0000

    initializes mongo, adds test collection

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -4,6 +4,7 @@ const userHelper    = require("../lib/util/user-helper")
 
 const express       = require('express');
 const tweetsRoutes  = express.Router();
+const moment        = require("moment");
 
 module.exports = function(DataHelpers) {
 
@@ -24,12 +25,13 @@ module.exports = function(DataHelpers) {
     }
 
     const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
+    const timeStamp = Date.now();
     const tweet = {
       user: user,
       content: {
         text: req.body.text
       },
-      created_at: Date.now()
+      created_at: moment(timeStamp).fromNow()
     };
 
     DataHelpers.saveTweet(tweet, (err) => {
